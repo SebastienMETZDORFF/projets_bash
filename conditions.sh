93fdbdb14a60a668a9ca22a93e0ee3e5aad2d947
Sebastien METZDORFF <sebmetzdorff@gmail.com>
2022-02-14 16:38:41 +0100

    Ajout des fichiers bash d'extension sh

---
+++ conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $1 = "Bruno" ]
+then
+	echo "Salut Bruno !"
+elif [ $1 = "Michel" ]
+then
+	echo "Salut Michel !"
+elif [ $1 = "Jean" ]
+then
+	echo "Salut Jean !"
+else
+	echo "Je ne te connais pas, ouste !"
+fi
---
+++ variables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+tableau=('valeur0' 'valeur1' 'valeur2')
+echo ${tableau[2]}
